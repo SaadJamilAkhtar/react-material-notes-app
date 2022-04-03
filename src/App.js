c995cb01265a25d4db3c87507e5fac6b83fc0b8d
MuhammadSaadJamil <muhammadsaadjamil1@gmail.com>
2022-04-04 01:31:50 +0500

    added theme

--- src/App.js
+++ src/App.js
@@ -3,19 +3,31 @@ import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Create from "./pages/Create";
 import Notes from "./pages/Notes";
 import NotFound from "./pages/NotFound";
+import {createTheme, ThemeProvider} from '@mui/material'
+import {purple} from '@mui/material/colors'
+
+const theme = createTheme({
+    palette: {
+        secondary: purple
+    }
+
+});
+
 function App() {
     return (
-        <Router>
-            <div className="App">
-                <div className="content">
-                    <Routes>
-                        <Route path={'/'} element={<Notes/>}/>
-                        <Route path={'/add'} element={<Create/>}/>
-                        <Route path={'*'} element={<NotFound/>}/>
-                    </Routes>
+        <ThemeProvider theme={theme}>
+            <Router>
+                <div className="App">
+                    <div className="content">
+                        <Routes>
+                            <Route path={'/'} element={<Notes/>}/>
+                            <Route path={'/add'} element={<Create/>}/>
+                            <Route path={'*'} element={<NotFound/>}/>
+                        </Routes>
+                    </div>
                 </div>
-            </div>
-        </Router>
+            </Router>
+        </ThemeProvider>
     );
 }
 
