c7bac5d242c481da5687dda3752332b8b42b8e07
MuhammadSaadJamil <muhammadsaadjamil1@gmail.com>
2022-04-07 02:42:57 +0500

    added links

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,6 +2,14 @@ import React from 'react';
 import {makeStyles} from "@mui/styles";
 import {Drawer} from "@mui/material";
 import {Typography} from "@mui/material";
+import {List} from "@mui/material";
+import {ListItem} from "@mui/material";
+import {ListItemText} from "@mui/material";
+import {ListItemIcon} from "@mui/material";
+import {useNavigate} from "react-router-dom";
+import {AddCircleOutline, SubjectOutlined} from "@mui/icons-material";
+import {purple} from '@mui/material/colors'
+import {useLocation} from "react-router-dom";
 
 const drawerWidth = 240;
 
@@ -19,11 +27,32 @@ const useStyle = makeStyles({
     },
     root: {
         display: "flex"
+    },
+    drawerTitle: {
+        paddingTop: '2rem',
+        paddingBottom: '2rem'
     }
 });
 
 function Layout({children}) {
     const classes = useStyle()
+    const navigate = useNavigate()
+    const location = useLocation();
+
+    const menuItems = [
+        {
+            text: "My Notes",
+            icon: <SubjectOutlined color={'secondary'}/>,
+            iconWhite: <SubjectOutlined style={{color: 'white'}}/>,
+            path: '/'
+        },
+        {
+            text: "Create Notes",
+            icon: <AddCircleOutline color={'secondary'}/>,
+            iconWhite: <AddCircleOutline style={{color: 'white'}}/>,
+            path: '/add'
+        }
+    ]
 
     return (
         <div className={classes.root}>
@@ -33,11 +62,33 @@ function Layout({children}) {
                 anchor={'left'}
                 classes={{paper: classes.drawerPaper}}
             >
-                <div>
-                    <Typography variant={'h5'}>
+                <div className={classes.drawerTitle}>
+                    <Typography variant={'h5'} align={'center'}>
                         My Notes
                     </Typography>
                 </div>
+
+                <List>
+                    {menuItems.map(item => (
+                        <ListItem
+                            key={item.text}
+                            button
+                            onClick={() => {
+                                navigate(item.path)
+                            }}
+                            style={location.pathname === item.path ? {
+                                background: '#D500F9',
+                                color: "white"
+                            } : null}
+                        >
+
+                            <ListItemIcon>{location.pathname === item.path ? item.iconWhite: item.icon}</ListItemIcon>
+                            <ListItemText>{item.text}</ListItemText>
+
+                        </ListItem>
+                    ))}
+                </List>
+
             </Drawer>
             <div className={classes.page}>
                 {children}
