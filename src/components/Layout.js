f4967f8906c96778034dd549b11ec9e65af1070a
MuhammadSaadJamil <muhammadsaadjamil1@gmail.com>
2022-04-07 02:07:59 +0500

    app drawer created

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import {makeStyles} from "@mui/styles";
+import {Drawer} from "@mui/material";
+import {Typography} from "@mui/material";
+
+const drawerWidth = 240;
 
 const useStyle = makeStyles({
-    page:{
+    page: {
         background: '#f9f9f9',
         width: '100%'
+    },
+    drawer: {
+        width: drawerWidth,
+
+    },
+    drawerPaper: {
+        width: drawerWidth
+    },
+    root: {
+        display: "flex"
     }
 });
 
@@ -12,7 +26,19 @@ function Layout({children}) {
     const classes = useStyle()
 
     return (
-        <div>
+        <div className={classes.root}>
+            <Drawer
+                className={classes.drawer}
+                variant={'permanent'}
+                anchor={'left'}
+                classes={{paper: classes.drawerPaper}}
+            >
+                <div>
+                    <Typography variant={'h5'}>
+                        My Notes
+                    </Typography>
+                </div>
+            </Drawer>
             <div className={classes.page}>
                 {children}
             </div>
